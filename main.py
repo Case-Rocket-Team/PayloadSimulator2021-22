6610bc6ef80df5369d63d59e769f84c10974d491
bgrywals <71354145+bgrywals@users.noreply.github.com>
2021-11-02 19:34:20 -0400

    Added graphing functionality

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from Kinetics import simulate_flight, get_wind_speed, get_air_density
-from icecream import ic
+#from icecream import ic
 import numpy as np
+from util import graph_data
 
 
 pos = np.array([0, 0, 1000])
 vel = np.array([0, 0, -20])
 vel_mag = np.linalg.norm(vel)
-heading = np.zeros(3)
+heading = np.array([0, 0.174533, 0])
 applied_acceleration = np.zeros(3)
 _mass = 4.249
 _timestep = .1
 
+sim_kinematics = []
 
 while pos[2] > 0:
     pos, heading, vel, vel_mag, accel = simulate_flight(_mass, pos, vel, vel_mag, heading, applied_acceleration, _timestep, get_air_density, get_wind_speed)
+    
+    sim_kinematics.append(pos)
+
+    '''
     print("\n\npos")
     print(pos)
     print("heading")
@@ -22,4 +28,6 @@ while pos[2] > 0:
     print(vel)
     print("accel")
     print(accel)
-    
+    '''
+
+graph_data(sim_kinematics, _timestep, 0, 0, 0)
