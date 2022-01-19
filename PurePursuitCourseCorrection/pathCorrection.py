4ebdd2d338fdabd63c18ca4104dcbcb39c671118
Kyler <kylerrosen@gmail.com>
2022-01-18 19:41:53 -0500

    Fixing Pure Pursuit datatypes

--- PurePursuitCourseCorrection/pathCorrection.py
+++ PurePursuitCourseCorrection/pathCorrection.py
@@ -1,33 +1,55 @@
 import math
-import numpy
+import numpy as np
 
-#actually make work at some point
-lastLooked = 0
+# actually make work at some point
+lastLooked = None
 
-def distFormula3D(pos, point):
-    return math.sqrt((pos.getX() - point.getX()) ** 2) + ((pos.getY() - point.getY()) ** 2) #+ (pos.getZ() - point.getZ()) ** 2)
 
-def main(pos, path, lookAheadDist, velocity):
+def dist_formula_2d(pos, point):
+    return math.sqrt((pos[0] - point[0]) ** 2) + ((pos[1] - point[1]) ** 2)  # + (pos.getZ() - point.getZ()) ** 2)
+
+
+def main(pos, look_ahead_distance, velocity):
+    # TODO: Last looked is a global variable that is the head of an updated linked list storing the optimal path
     global lastLooked
     prev = lastLooked
-    #traversal variable
+    # traversal variable
     trav = lastLooked.getNext()
 
-    while distFormula3D(pos, trav) < distFormula3D(pos, prev):
+    while dist_formula_2d(pos, trav) < dist_formula_2d(pos, prev):
         prev = trav
         trav = trav.getNext()
-    while distFormula3D(pos, trav) < lookAheadDist:
+    while dist_formula_2d(pos, trav) < look_ahead_distance:
         prev = trav
         trav = trav.getNext()
 
-    #finding distance between us and where we want to go
-    #(pos.getX() - trav.getX() + (pos.getY() - trav.getY() + #(pos.getZ() - trav.getZ()) 
-    #invverse (cos u . v) / (mag u) (mag v)
-    math.acos(numpy.dot(a, b)[] / 
-    #rotation matrix code
-    rotX = (point.getX() * cos(angle)) - (point.getY() * sin(angle))
-    #rotY = (point.getX() * sin(angle)) + (point.getY() * cos(angle))
+    lastLooked = trav
+    # finding distance between us and where we want to go
+    # (pos[0] - trav[0]) + (pos[1]] - trav[1]] + #(pos.getZ() - trav.getZ())
+    # inverse (cos u . v) / (mag u) (mag v)
 
+    # making vector 1 2d
+    heading = velocity[:2]
+    unit_heading = heading / np.linalg.norm(heading)
+
+    unit_y_axis = [1, 0]
+
+    dot_product = np.dot(unit_heading, unit_y_axis)
+    angle = np.arccos(dot_product)
+
+    point = trav[:2] - pos[:2]
+
+    # rotation matrix code
+    rotX = (point[0] * np.cos(angle)) - (point[1] * np.sin(angle))
+    # rotY = (point[0]] * np.sin(angle)) + (point[1]] * np.cos(angle))
+
+    radius = (dist_formula_2d(pos, trav.getKey())) / 2 * rotX
+
+    # TODO: determine number of waypoints to return
+    num_points_created = 10
+    # CREATES WAYPOINTS
+
+    return lastLooked, [(pos[0] + trav.getKey()[0] - (radius**2 - y**2), pos[1] + y) for y in range(num_points_created)]
 
 
 main()
