31586190a19147707a95a7dd1928277a0d0a433e
Kyler <kylerrosen@gmail.com>
2022-02-09 02:08:34 -0500

    v0.1 of Path Generation

--- ControlLoop.py
+++ ControlLoop.py
@@ -1,8 +1,20 @@
+class Node:
+    # Function to initialise the node object
+    def __init__(self, data, next_node=None):
+        self.data = data  # Assign data
+        self.next = next_node  # Initialize next as null
+
+    def get_next(self):
+        return self.next
+
+    def get_value(self):
+        return self.data
+
 def dead_reckon(gps,imu,pressure,pos_prev,vel_prev,accel_prev):
     """integrate position from of the craft from sensor data
 
     Args:
-        gps (Vec2): the generated noisy gps data    
+        gps (Vec2): the generated noisy gps data
         imu (Vec3): the generated noisy imu accelerations
         pressure (float): the generated noisy altitude data
         pos_prev (Vec3[]): a history of the previous positions
@@ -30,6 +42,9 @@ def pure_pursuit(pos,vel,accel,path):
     """
     return accel_hat
 
+def cross_product(a, b, c):
+    return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[1] - a[1])
+
 def gen_path(pos, vel, turn_radius, target_loc, num_waypoints):
     """Generate a set of waypoints for the craft to follow
 
@@ -43,4 +58,63 @@ def gen_path(pos, vel, turn_radius, target_loc, num_waypoints):
     Returns:
         path: the set of waypoints generated
     """
+    # find whether distance is shorter turning left or right
+    turn_right = True
+
+    if cross_product([0,0], velocity, target_loc) > 0:
+        turn_right = False
+
+    # while tangent of minimum radius circle does not point towards target:
+    # move 1 step farther on circle
+    # reminder that in a circle, dy/dx = -x/y
+    dx = velocity[0]
+    dy = velocity[1]
+    tangent_slope = dy/dx
+
+    # dy/dx = -x/y so x = -dy/dx * y
+    # r = sqrt((x-x_0)^2 + (y-y_0)y^2), but x,y = 0,0 so r = sqrt((x_0)^2 + (y_0)^2)
+    # rewrite r = sqrt((x_0)^2 + (y_0)^2) = sqrt((-dy/dx * y_0)^2 + (y_0)^2) = y_0 * sqrt((dy/dx)^2 + 1)
+    y_0 = turn_radius / sqrt(1 + tangent_slope**2)
+    x_0 = turn_radius**2 - y_0**2
+
+    # see this answer for finding the tangent line off a circle that intersects a point
+    # https://www.quora.com/What-is-the-point-of-intersection-of-the-tangents-drawn-at-the-points-where-the-given-line-intersects-the-given-circle
+
+    radius_to_target = np.subtract(target_loc, [x_0, y_0])
+    p = sqrt(radius_to_target[0]**2 + radius_to_target[1]**2)
+
+    c = turn_radius**2 - p
+    h = sqrt(turn_radius**2 - c**2)
+
+    norm_radius_to_target = np.linalg.norm(radius_to_target)
+    c = c * norm_radius_to_target
+    h = h * np.rot90(velocity)
+    delta = np.add(c, h)
+
+    tangent_point = np.add([x_0, y_0], delta)
+
+    if cross_product([0, 0], np.rot90(velocity), tangent_point) < 0:
+        h = -2 * h
+        delta = np.subtract(c, h)
+        tangent_point = np.add([x_0, y_0], delta)
+
+    # final vector saved in tangent_point
+
+    straight_path_direction = np.substract(target_loc, pos)
+    norm_straight_path_direction = np.linalg.norm(straight_path_direction)
+
+    # generate path between the tangent point and the target point
+    step = 10
+    straight_path = []
+
+    for i in range(0, sqrt(straight_path_direction[0]**2 + straight_path_direction[1]**2), step):
+        straight.append([target_loc + i * norm_straight_path_direction])
+
+    error_margin = 10
+    guess_r = 2 * turn_radius
+    expected_height = 7
+    # TODO: find expected height at that point
+
+    while abs(expected_height % 2 * pi * guess_r * loop_necessary * (dr / dt)) > error_margin:
+
     return path
